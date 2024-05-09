727ee40bf4fea478f45ea920983222a910ffc2fb
Krutzzio <gerard.costa.curr@gmail.com>
2024-05-09 22:10:11 +0200

    comit

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			"background": "#E4E2DD",
+			"primary": "#282726",
+			"secondary": "#DB4A2B",
+		},
+		extend: {},
+	},
+	plugins: [],
+}
